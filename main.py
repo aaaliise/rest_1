8fd3cfbc6fa738aa0c3f11368b8aab7cc2a53d62
Alice <149301249+aaaliise@users.noreply.github.com>
2024-04-26 19:50:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask_restful import reqparse, abort, Api, Resource
+from flask import Flask
+from data import users_resources
+from data import db_session
+
+app = Flask(__name__)
+api = Api(app)
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    api.add_resource(users_resources.UsersListResource, '/api/v2/users')
+    api.add_resource(users_resources.UsersResource, '/api/v2/users/<users_id>')
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
